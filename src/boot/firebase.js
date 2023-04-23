863590ed869a6d533fe5495ef924757499dabd89
mayrigon <mayrarfls@gmail.com>
2023-04-23 18:43:42 -0300

    firsrt commit

---
+++ src/boot/firebase.js
@@ -0,0 +1,29 @@
+// import { boot } from 'quasar/wrappers'
+
+import { initializeApp } from "firebase/app"
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5wVa9CWUCM0BEhzZKzuITjxO2CZjMvQQ",
+  authDomain: "chat-project-7543d.firebaseapp.com",
+  projectId: "chat-project-7543d",
+  storageBucket: "chat-project-7543d.appspot.com",
+  messagingSenderId: "88275249925",
+  appId: "1:88275249925:web:070c23470a247c8959e56c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+const marcarTempo = firebase.firestore.FieldValue.serverTimestamp
+
+export { db, auth, marcarTempo }
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+// export default boot(async (/* { app, router, ... } */) => {
+//   // something to do
+// })
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Chat
+        </q-toolbar-title>
+
+        <div>
+          <q-btn color="negative">Sair</q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    
+  },
+
+  setup () {
+    
+
+    return {
+      
+    }
+  }
+}
+</script>
