2ddc97ddd1eef87b04e77e737b4ca538c3f7a7ea
mayrigon <mayrarfls@gmail.com>
2023-05-02 18:04:02 -0300

    component IndexPage created

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,35 @@
 <template>
   <q-page padding>
-    
+    <UserAccess v-if="!user" />
   </q-page>
 </template>
 
 <script>
+import UserAccess from "../components/UserAccess.vue";
+import { getAuth } from "firebase/auth";
+import { ref } from "vue";
 
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+
+  components: { UserAccess },
+  setup() {
+    const auth = getAuth();
+    const user = ref(null);
+
+    auth.onAuthStateChanged((dadosDoUsuarioLogado) => {
+      if (dadosDoUsuarioLogado) {
+        const uid = dadosDoUsuarioLogado.uid;
+        user.value = dadosDoUsuarioLogado;
+      } else {
+        user.value = null;
+        console.log("O usuário está desconectado");
+      }
+    });
+
+    return {
+      user,
+    };
+  },
+};
 </script>
